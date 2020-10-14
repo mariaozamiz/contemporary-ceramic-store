6a351b8a4bf2b32a450705c11876ceadf241a11d
Maria Ozamiz <ozamiz.m@gmail.com>
2020-10-14 17:41:51 +0200

    add image

--- src/components/directory-menu/Directory-menu.js
+++ src/components/directory-menu/Directory-menu.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuItem from '../menu-item/Menu-item.js';
 import './directory-menu.scss';
 
 function DirectoryMenu() {
-    return (
-        <div className="directory-menu">
-            <MenuItem />
-        </div>
-    );
+    const [section, setSection] = useState([
+        { title: 'vases', imageUrl: 'h', id: 1 },
+        { title: 'miscellaneous', imageUrl: 'h', id: 2 },
+        { title: 'bowls', imageUrl: 'h', id: 3 },
+        { title: 'home', imageUrl: 'h', id: 4 },
+        { title: 'tableware', imageUrl: 'h', id: 5 },
+    ]);
+
+    const sections = section.map((section) => {
+        return (
+            <MenuItem
+                key={section.id}
+                title={section.title}
+                imageUrl={section.imageUrl}
+                id={section.id}
+            />
+        );
+    });
+
+    return <div className="directory-menu">{sections}</div>;
 }
 
 export default DirectoryMenu;
