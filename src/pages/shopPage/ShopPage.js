2f5c1c2722b01fcf25c250adde724726553c099c
Maria Ozamiz <ozamiz.m@gmail.com>
2020-11-06 19:09:23 +0100

    create shop page

---
+++ src/pages/shopPage/ShopPage.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import './shoppage.scss';
+
+function ShopPage() {
+    const [collections, setCollections] = useState([]);
+
+    // useEffect(() => {}, []);
+
+    return (
+        <div>
+            <h1>Shop here!</h1>
+        </div>
+    );
+}
+
+export default ShopPage;
