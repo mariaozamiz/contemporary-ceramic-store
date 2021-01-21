a482aaa4c157d82002a0d23d052bf4f64d21a314
Maria Ozamiz <ozamiz.m@gmail.com>
2021-01-21 18:55:12 +0100

    added persistStore

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate, persistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
     <Provider store={store}>
         <HashRouter>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </HashRouter>
     </Provider>,
     document.getElementById('root')
--- src/pages/checkoutPage/CheckoutPage.js
+++ src/pages/checkoutPage/CheckoutPage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import {
-    selectCartHidden,
     selectCartItems,
     selectCartTotal,
 } from '../../redux/cart/cart.selectors';
